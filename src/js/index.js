58b9536ed7493f0d70908b6f6e3fbba3c90d0fcd
DC0507 <dcalderon179105@gmail.com>
2021-03-04 01:47:37 +0000

    Exercise Completedgit status

--- src/js/index.js
+++ src/js/index.js
@@ -28,19 +28,27 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-
   // reset the website body with the new html output
+  if (variables.name == null) variables.name = "Name";
+  if (variables.lastname == null) variables.lastname = "Lastname";
+  if (variables.role == null) variables.role = "Role";
+  if (variables.city == null) variables.city = "City";
+  if (variables.country == null) variables.country = "Country";
+  if (variables.twitter == null) variables.twitter = "https://twitter.com";
+  if (variables.github == null) variables.github = "https://github.com";
+  if (variables.linkedin == null)
+    variables.instagram = "https://www.instagram.com";
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+          <h1>${variables.name} ${variables.lastname}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}, ${variables.country}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="${variables.twitter}"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="${variables.github}"><i class="fa fa-github"></i></a></li>
+            <li><a href="${variables.linkedin}"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="${variables.instagram}"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -54,9 +62,12 @@ window.onload = function() {
     // if includeCover is true the algorithm should
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://resources.owllabs.com/hubfs/bloomscape-zoom-2-jpg.jpeg",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL: `https://randomuser.me/api/portraits/women/${Math.floor(
+      Math.random() * 100
+    )}.jpg`,
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
